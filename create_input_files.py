7cd8b50e96fda4b2044e488f7e23231023ce2234
KP <31988529+katsamapol@users.noreply.github.com>
2022-12-18 10:17:09 -0500

    Update create_input_files.py

--- create_input_files.py
+++ create_input_files.py
@@ -1,10 +1,31 @@
+import argparse
 from utils import create_input_files
+from params_class import *
 
+
+def _parse_arguments():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("-d", "--which_data", default="coco2014", type=str, 
+    help="Which dataset to use 'coco2014', 'flickr8k'", choices=["coco2014", "flickr8k"])
+
+    return argparser.parse_args()
+    
 if __name__ == '__main__':
-    create_input_files(dataset='coco',
-                      karpathy_json_path='/data/dataset_coco.json',
-                      image_folder='/data/images/',
-                      captions_per_image=5,
-                      min_word_freq=5,
-                      output_folder='/data/processed_HDF5/COCO2014/',
-                      max_len=50)
+    # Create input files (along with word map)
+    args = _parse_arguments()
+    if(args.which_data == "coco2014"):
+        create_input_files(dataset='coco',
+                        karpathy_json_path=data_path+'caption_datasets/dataset_coco.json',
+                        image_folder=data_path+'images/',
+                        captions_per_image=5,
+                        min_word_freq=5,
+                        output_folder=data_path+'processed_HDF5/COCO2014/',
+                        max_len=50)
+    elif(args.which_data == "flickr8k"):
+        create_input_files(dataset='flickr8k',
+                        karpathy_json_path=data_path+'caption_datasets/dataset_flickr8k.json',
+                        image_folder=data_path+'images/Flicker8k_Dataset/',
+                        captions_per_image=5,
+                        min_word_freq=5,
+                        output_folder=data_path+'processed_HDF5/Flicker8k_Dataset/',
+                        max_len=50)
