9742c79d1c44983f971726819e80807b3f60c9bc
KP <31988529+katsamapol@users.noreply.github.com>
2022-12-18 09:55:25 -0500

    Update create_input_files.py

--- create_input_files.py
+++ create_input_files.py
@@ -1,25 +1,10 @@
 from utils import create_input_files
 
 if __name__ == '__main__':
-    # Create input files (along with word map)
-    # create_input_files(dataset='coco',
-    #                   karpathy_json_path='/scratch/ps4534/ml/image-captioning/data/karpathy_caption_datasets/dataset_coco.json',
-    #                   image_folder='/scratch/ps4534/ml/adversarial-attack-to-caption/data/images/',
-    #                   captions_per_image=5,
-    #                   min_word_freq=5,
-    #                   output_folder='/scratch/ps4534/ml/image-captioning/data/HDF5/',
-    #                   max_len=50)
-    # create_input_files(dataset='flickr8k',
-    #                    karpathy_json_path='/scratch/ps4534/ml/data/captions/dataset_flickr8k.json',
-    #                    image_folder='/scratch/ps4534/ml/data/images/Flicker8k_Dataset/',
-    #                    captions_per_image=5,
-    #                    min_word_freq=5,
-    #                    output_folder='/scratch/ps4534/ml/data/processed_HDF5/Flicker8k_Dataset/',
-    #                    max_len=50)
     create_input_files(dataset='coco',
-                      karpathy_json_path='/scratch/ps4534/ml/image-captioning/data/karpathy_caption_datasets/dataset_coco.json',
-                      image_folder='/scratch/ps4534/ml/adversarial-attack-to-caption/data/images/',
+                      karpathy_json_path='/data/dataset_coco.json',
+                      image_folder='/data/images/',
                       captions_per_image=5,
                       min_word_freq=5,
-                      output_folder='/scratch/ps4534/ml/data/processed_HDF5/TEST_EXPORT/',
+                      output_folder='/data/processed_HDF5/COCO2014/',
                       max_len=50)
