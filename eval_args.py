817eee17d18a4c589d5f07a68c8036b1913aeeae
KP <31988529+katsamapol@users.noreply.github.com>
2022-12-18 21:53:59 -0500

    Update eval_args.py

--- eval_args.py
+++ eval_args.py
@@ -240,12 +240,12 @@ def evaluate(beam_size):
 
 def _parse_arguments():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("-m", "--which_model", default="resnet101", type=str, 
+    argparser.add_argument("-m", "--which_model", type=str, 
     help="Which model to use 'resnet50', 'resnet101', or 'resnet152'", choices=["resnet50","resnet101", "resnet152"])
-    argparser.add_argument("-d", "--which_data", default="coco2014", type=str, 
-    help="Which dataset to use 'coco2014', 'flickr8k', 'flickr30k'", choices=["coco2014", "flickr8k", "flickr30k"])
+    argparser.add_argument("-d", "--which_data", type=str, 
+    help="Which dataset to use 'coco2014', or 'flickr8k'", choices=["coco2014", "flickr8k"])
     argparser.add_argument("-b", "--beam_size", default=3, type=int,
-    help="Beam size at which to generate captions for evaluation", choices=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+    help="Beam size at which to generate captions for evaluation", choices=[1, 2, 3, 4, 5, 6, 7, 8])
     return argparser.parse_args()
 
 if __name__ == "__main__":
